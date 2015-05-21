3383de9c2c82adcabfa999490637370a7c1e2407
John Bellone <john.bellone.jr@gmail.com>
2015-05-21 17:31:50 -0400

    Update the specs for libraries to look like they work.

--- test/spec/libraries/consul_config_spec.rb
+++ test/spec/libraries/consul_config_spec.rb
@@ -1,4 +1,23 @@
 require 'spec_helper'
 
 describe_resource 'consul_config' do
+  context 'with default attributes' do
+    let(:cookbook_name) { 'consul::default' }
+
+    it do
+      expect(chef_run).to create_directory('/etc/consul.d')
+        .with(owner: 'consul')
+        .with(group: 'consul')
+    end
+
+    it do
+      expect(chef_run).to create_file('/etc/consul.d/default.json')
+        .with(owner: 'consul')
+        .with(group: 'consul')
+    end
+
+    it 'converges successfully' do
+      chef_run
+    end
+  end
 end
--- test/spec/libraries/consul_service_spec.rb
+++ test/spec/libraries/consul_service_spec.rb
@@ -1,4 +1,9 @@
 require 'spec_helper'
 
 describe_resource 'consul_service' do
+  context 'with default attributes' do
+    it 'converges successfully' do
+      chef_run
+    end
+  end
 end
